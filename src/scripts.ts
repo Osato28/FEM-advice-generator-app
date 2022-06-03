d3be4e517d0baadc0651d71410cb895c032819de
Osato28 <65938107+Osato28@users.noreply.github.com>
2022-06-03 20:04:19 +0300

    Add template-cloning to ensure noscript's proper functioning

--- src/scripts.ts
+++ src/scripts.ts
@@ -1,25 +1,39 @@
-let adviceID = document.getElementById('adviceID');
-let adviceText = document.getElementById('adviceText');
-let rerollButton = document.getElementById('rerollButton')
+
+// Encapsulated one-use variables in the function so they'll get garbage-cleaned after usage
+
+function cloneTemplateToWrapper(templateID: string, wrapperID: string) {
+    /* Note: TypeScript got REALLY prissy about template.content, so it needs a lot of typecasting before it'll compile */
+    const template = document.getElementById(templateID) as HTMLMetaElement | null;
+    const templateContent = template!.content as unknown as Node;
+    const clone = templateContent.cloneNode(true);
+
+    const componentWrapper = document.getElementById(wrapperID);
+    componentWrapper?.appendChild(clone);
+}
+
+cloneTemplateToWrapper('adviceTemplate', 'componentWrapper');
+
+// After template is loaded, initiate pointers to elements
+
+const adviceID = document.getElementById('adviceID');
+const adviceText = document.getElementById('adviceText');
+const rerollButton = document.getElementById('rerollButton')
 
 function fetchAdvice(id:number = -1) {
-    adviceID!.innerText = '...';
-    adviceText!.innerText = 'Loading...';
-    // if id < 0 {
-        fetch('https://api.adviceslip.com/advice')
-        .then(response => response.json())
-        .then(data => {
-            adviceID!.innerText = data.slip.id;
-            adviceText!.innerText = data.slip.advice;
-            adviceText!.focus();
-        })
-        .catch(error => console.log(error));
-    // }
+    fetch('https://api.adviceslip.com/advice')
+    .then(response => response.json())
+    .then(data => {
+        adviceID!.innerText = data.slip.id;
+        adviceText!.innerText = data.slip.advice;
+
+        adviceText!.focus();
+    })
+    .catch(error => console.log(error));
 }
 
 fetchAdvice();
 
-// TODO: process button onClick: fetch new advice, THEN return focus to adviceText
+// And add event handlers
 
 rerollButton?.addEventListener('click', (ev) => {
     fetchAdvice();
